e7d718ca704224f90a1a9434c80970d336dcf9da
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-19 15:41:29 -0700

    added clustering and celltype detection

--- code/rna_preprocessing/02_cluster_annotate.R
+++ code/rna_preprocessing/02_cluster_annotate.R
@@ -9,34 +9,78 @@ library(dplyr)
 library(harmony)
 library(stringr)
 set.seed(42555)
+
+source("code/utils/rna_utils.R")
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                Import data                               ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 seurat <- qread("data/processed/filtered_seurat.qs")
 
-load("data/metadata/cycle.rda") # cell cycle
+load("/media/largedata/universal_files/cycle.rda") # cell cycle
 
 ## split into CD3 and CD45 groups ---------------------------------------------
-sc_cd3 <- subset(seurat, flow_cell == "CD3")
-sc_cd45 <- subset(seurat, flow_cell == "CD45")
+# sc_cd3 <- subset(seurat, flow_cell == "CD3")
+# sc_cd45 <- subset(seurat, flow_cell == "CD45")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                            Normalize and Cluster                         ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## CD3 clustering --------------------------------------------------------------
-sc_cd3 <- sc_cd3 %>% NormalizeData()
-sc_cd3 <- sc_cd3 %>% CellCycleScoring(s.features = s_genes, g2m.features = g2m_genes)
-sc_cd3 <- sc_cd3 %>% FindVariableFeatures()
-sc_cd3 <- sc_cd3 %>% ScaleData()
-sc_cd3 <- sc_cd3 %>% RunPCA()
+seurat <- seurat %>% NormalizeData()
+seurat <- seurat %>% CellCycleScoring(s.features = s_genes, g2m.features = g2m_genes)
+seurat <- seurat %>% FindVariableFeatures()
+seurat <- seurat %>% ScaleData()
+seurat <- seurat %>% RunPCA()
+seurat <- seurat %>% RunUMAP(reduction = "pca", dims = 1:40)
 
-sc_cd3 <- sc_cd3 %>% RunUMAP(reduction = "pca", dims = 1:40)
-DimPlot(sc_cd3, group.by = c("patient_group"))
-DimPlot(sc_cd3, group.by = c("sample", "Phase"))
-DimPlot(sc_cd3, group.by = c("scDbl_class","doubletfind_class"))
+seurat <- seurat %>% FindNeighbors(reduction = "pca", dims = 1:40)
+seurat <- seurat %>% FindClusters(resolution = c(0.4))
 
-FeaturePlot(sc_cd3, min.cutoff = "q1", max.cutoff = "q99", features = c("CD3E","CD3D","CD3G"))
-FeaturePlot(sc_cd3, min.cutoff = "q1", max.cutoff = "q99", 
+### basic plotting ------------------------------------------------------------
+DimPlot(seurat, group.by = c("flow_cell"))
+DimPlot(seurat, group.by = c("patient_group"))
+DimPlot(seurat, group.by = c("sample", "Phase"))
+FeaturePlot(seurat, min.cutoff = "q1", max.cutoff = "q99", 
             features = c("nCount_RNA","nFeature_RNA","mitoRatio","log10GenesPerUMI"))
 
-### remove doublets and 
+### broad celltype marker plotting -------------------------------------------
+DimPlot(seurat, group.by = c("RNA_snn_res.0.4"), label = TRUE)
+
+panglao_scores <- score_panglao(seurat)
+
+manuscript_markers <- list()
+manuscript_markers[["plasma"]] <- c("IGHA1","IGHG1")
+manuscript_markers[["mast"]] <- c("TPSAB1")
+manuscript_markers[["myeloid"]] <- c("S100A9","C1QC")
+manuscript_markers[["B_cell"]] <- c("MS4A1","FCER2","MEF2B")
+manuscript_markers[["ILC"]] <- c("CSF2","IL7R","GZMB")
+
+FeaturePlot(seurat, min.cutoff = "q1", max.cutoff = "q99", 
+            features = manuscript_markers[["ILC"]])
+
+seurat$annotation_level1 <- revalue(seurat$SCT_snn_res.1, 
+                                    c('0' = "",
+                                      '1' = "",
+                                      '2' = "",
+                                      '3' = "",
+                                      '4' = "",
+                                      '5' = "",
+                                      '6' = "",
+                                      '7' = "",
+                                      '8' = "",
+                                      '9' = "",
+                                      '10' = "",
+                                      '11' = "",
+                                      '12' = "",
+                                      '13' = "",
+                                      '14' = "",
+                                      '15' = "", 
+                                      '16' = "",
+                                      '17' = "",
+                                      '18' = "",
+                                      '19' = "",
+                                      '20' = "",
+                                      '21' = "",
+                                      '22' = ""
+
+
+
