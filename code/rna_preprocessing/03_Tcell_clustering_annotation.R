dce612bf59b5c590df73e66fe6d3345b67d92470
Scott Ginebaugh <spginebaugh@gmail.com>
2024-09-22 12:54:10 -0700

    added cd4 subtypes

--- code/rna_preprocessing/03_Tcell_clustering_annotation.R
+++ code/rna_preprocessing/03_Tcell_clustering_annotation.R
@@ -82,3 +82,90 @@ seurat$azimuth_celltype_3 <- sc_azimuth$predicted.celltype.l3
 ## will use azimuth celltype level1 to select CD8 and CD4
 
 qsave(seurat, "data/processed/clustered_tcell.qs")
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                               subcluster CD4                             ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd4 <- seurat[,seurat$azimuth_celltype_1 == "CD4 T"]
+cd4 <- cd4 %>% FindVariableFeatures()
+cd4 <- cd4 %>% ScaleData()
+cd4 <- cd4 %>% RunPCA()
+cd4 <- cd4 %>% RunHarmony(group.by.vars = "sample", dims.use = 1:30)
+cd4 <- cd4 %>% RunUMAP(reduction = "harmony", dims = 1:30)
+cd4 <- cd4 %>% FindNeighbors(reduction = "harmony", dims = 1:30)
+cd4 <- cd4 %>% FindClusters(resolution = c(0.6))
+
+
+DimPlot(cd4, group.by = c("flow_cell"))
+DimPlot(cd4, group.by = c("patient_group"))
+DimPlot(cd4, group.by = c("sample", "Phase"))
+FeaturePlot(cd4, min.cutoff = "q1", max.cutoff = "q99", 
+            features = c("nCount_RNA","nFeature_RNA","mitoRatio","log10GenesPerUMI"))
+DimPlot(cd4, group.by = c("RNA_snn_res.0.6", "azimuth_celltype_2", "azimuth_celltype_3"), label = TRUE)
+
+cd4_subtype_markers <- c('IL4I1','IL23R','NR4A1','EGR1','CCL5','ANXA1','S1PR1','KLF2',
+                         'CCR7','TOX2','DRAIC','IKZF2','FOXP3','GZMH','GZMK','GBP5',
+                         'GPR25','MKI67','TYMS')
+
+DimPlot(cd4, group.by = c("RNA_snn_res.0.6"), label = TRUE)
+VlnPlot(cd4, features = cd4_subtype_markers, split.by = "RNA_snn_res.0.6", stack = TRUE, pt.size = 0)
+
+
+cd4$annotation_level3 <- plyr::revalue(cd4$RNA_snn_res.0.6, 
+                                    c('0' = "Th1_Trm_R",
+                                      '1' = "Th1_Trm_A",
+                                      '2' = "Th1_effector",
+                                      '3' = "Th17_Trm",
+                                      '4' = "Tfh",
+                                      '5' = "Central_memory",
+                                      '6' = "Treg",
+                                      '7' = "Naive",
+                                      '8' = "Naive",
+                                      '9' = "Cytotoxic",
+                                      '10' = "Cycling",
+                                      '11' = "Th1_Trm_R",
+                                      '12' = "Tcm_1",
+                                      '13' = "Tcm_2",
+                                      '14' = "Th1_effector",
+                                      '15' = "Treg"))
+DimPlot(cd4, group.by = "annotation_level3")
+qsave(cd4, "data/processed/annotated_cd4.qs")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                               subcluster CD8                             ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd8 <- seurat[,seurat$azimuth_celltype_1 == "CD8 T"]
+cd8 <- cd8 %>% FindVariableFeatures()
+cd8 <- cd8 %>% ScaleData()
+cd8 <- cd8 %>% RunPCA()
+cd8 <- cd8 %>% RunHarmony(group.by.vars = "sample", dims.use = 1:30)
+cd8 <- cd8 %>% RunUMAP(reduction = "harmony", dims = 1:30)
+cd8 <- cd8 %>% FindNeighbors(reduction = "harmony", dims = 1:30)
+cd8 <- cd8 %>% FindClusters(resolution = c(0.6))
+
+
+DimPlot(cd8, group.by = c("flow_cell"))
+DimPlot(cd8, group.by = c("patient_group"))
+DimPlot(cd8, group.by = c("sample", "Phase"))
+FeaturePlot(cd8, min.cutoff = "q1", max.cutoff = "q99", 
+            features = c("nCount_RNA","nFeature_RNA","mitoRatio","log10GenesPerUMI"))
+DimPlot(cd8, group.by = c("RNA_snn_res.1","RNA_snn_res.0.6"), label = TRUE)
+DimPlot(cd8, group.by = c("RNA_snn_res.0.6", "azimuth_celltype_2", "azimuth_celltype_3"), label = TRUE)
+
+cd8_subtype_markers <- c('KIR2DL4','TRDC','ANXA1','IL7R','GZMK','EOMES',
+                         'SLC4A10','KLRB1','MIAT','RNF213','CCR7','KLF2',
+                         'UCP2','GZMB','TYMS','MKI67')
+
+DimPlot(cd8, group.by = c("RNA_snn_res.0.6"), label = TRUE)
+VlnPlot(cd8, features = cd8_subtype_markers, split.by = "RNA_snn_res.0.6", stack = TRUE, pt.size = 0)
+
+
+
+
+
+
+
+
+
+
