0efe785893ebb1c72679a5c60c0b1bc3c462d05f
Scott Ginebaugh <spginebaugh@gmail.com>
2024-11-18 16:09:06 -0800

    Added documentation

--- code/utils/TCR_utils.R
+++ code/utils/TCR_utils.R
@@ -1,3 +1,4 @@
+# pulls information from seurat_obj barcode name and adds it to seurat_obj@meta.data
 add_contig_barcodes <- function(seurat_obj){
   seurat_obj$donor_id <- word(seurat_obj$sample,2,2,"_") %>% word(.,1,1,"-")
   barcode_stripped <- word(seurat_obj$barcode,-1,-1,"_") %>% word(.,1,1,"-")
